da21ea601dcdf9ac0284f186f610fec33d5e4fd9
Renitha <renithasarima@gmail.com>
2024-03-22 17:16:23 +0700

    add bubble sort

--- src/DataList.java
+++ src/DataList.java
@@ -15,4 +15,19 @@ public class DataList {
   public List<Data> getDataList() {
     return dataList;
   }
+
+// Algoritma pengurutan menggunakan Bubble Sort
+  public void bubbleSort() {
+    int n = dataList.size();
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (dataList.get(j).getValue() > dataList.get(j + 1).getValue()) {
+                // Tukar posisi elemen jika dalam urutan yang salah
+                Data temp = dataList.get(j);
+                dataList.set(j, dataList.get(j + 1));
+                dataList.set(j + 1, temp);
+            }
+        }
+    }
+  }
 }
